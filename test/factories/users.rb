378da9d3f1a4375658e2cd4b0be0e13bb3190df9
Brenda Wallace <brenda.wallace@dia.govt.nz>
2018-11-21 09:58:46 +1300

    Faker data for factorybot

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
-  factory :user do
-    
+  factory :user, aliases: [:owner] do
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
+    # after(:create) do |user, _evaluator|
+    #   user.confirm
+    # end
   end
 end
