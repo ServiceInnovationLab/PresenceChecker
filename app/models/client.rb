035b8ab75d33a6dd0916763ff449ab19f39160ef
Brenda Wallace <brenda.wallace@dia.govt.nz>
2018-12-05 16:32:12 +1300

    Rubocop happiness

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Client < ApplicationRecord
   has_many :identities, dependent: :destroy
   has_many :movements, through: :identities
--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Identity < ApplicationRecord
   belongs_to :client
   has_many :movements, dependent: :destroy
