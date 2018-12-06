abb574ca570414d5c30b27fe251e121a56d2c199
Brenda Wallace <brenda.wallace@dia.govt.nz>
2018-12-07 10:56:13 +1300

    Removed unused user controller method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,21 +7,6 @@ class UsersController < ApplicationController
     @users = User.order(:email)
   end
 
-  def edit; end
-
-  def update
-    if params[:activate].present?
-      @user.restore
-      redirect_to edit_user_url(@user), notice: 'User was re-actived.'
-    else
-      if @user.update(user_params)
-        redirect_to users_url, notice: 'User was updated.'
-      else
-        render :edit
-      end
-    end
-  end
-
   def destroy
     @user.destroy
     redirect_to users_url, notice: 'User was deleted.'
@@ -31,12 +16,5 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
-  end
-
-  def user_params
-    u = params.require(:user).permit(:council_id, role_ids: [])
-    u[:council_id] = nil if u[:council_id].blank?
-    u[:role_ids] = [] if u[:role_ids].blank?
-    u
   end
 end
