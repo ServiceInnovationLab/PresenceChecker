a6b833289b80f6c676ae834907f4dd8bf880ca47
Brenda Wallace <brenda.wallace@dia.govt.nz>
2018-12-06 16:40:57 +1300

    Removed unused code, and moved clients to the clients controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :movements
-  resources :identities
-  resources :clients
+  resources :clients, only: [:index]
   devise_for :users
-  resources :home
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root to: 'home#index'
+  root to: 'clients#index'
 end
