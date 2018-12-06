4f9ac53808fa7e6dc433ccfa7effd84bd4ff5066
Brenda Wallace <brenda.wallace@dia.govt.nz>
2018-12-06 16:46:41 +1300

    Factory for client

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,28 +3,4 @@
 require 'faker'
 include Faker
 
-5.times do
-  Client.create(
-    passport_no: Bank.iban('be')
-  )
-end
-
-5.times do |i|
-  Identity.create(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    second_name: Faker::Name.middle_name,
-    third_name: '',
-    date_of_birth: '2001-09-10',
-    country_of_birth: Faker::Address.country,
-    client_id: i + 1
-  )
-end
-
-5.times do |i|
-  Movement.create(
-    arrival_date: 8.days.ago,
-    departure_date: 1.day.ago,
-    client_id: i
-  )
-end
+FactoryBot.create_list :client, 5
