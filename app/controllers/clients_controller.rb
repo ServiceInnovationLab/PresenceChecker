71351e9aa1cf4e57f7511360daccc359bcc2d8b8
Awesome Code <team@xinminlabs.com>
2018-12-07 18:54:09 +0000

    Auto corrected by following Ruby GeneralLayout

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -15,7 +15,7 @@ class ClientsController < ApplicationController
   private
 
   # Use callbacks to share common setup or constraints between actions.
-  def set_client
-    @client = Client.find(params[:id])
-  end
+    def set_client
+      @client = Client.find(params[:id])
+    end
 end
