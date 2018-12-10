65f4704bd28ea407578a8354ea25dabe3f5e7b6a
Brenda Wallace <brenda.wallace@dia.govt.nz>
2018-12-11 12:56:07 +1300

    Service for a day

--- app/services/eligibility_service.rb
+++ app/services/eligibility_service.rb
@@ -4,32 +4,47 @@ class EligibilityService
   def initialize(client, day)
     @client = client
     @day = day
-    response = calculate(query)
   end
 
-  def meets_presence_requirements
-    response['persons'][person_name]['citizenship__meets_each_year_minimum_presence_requirements']
+  def run!
+    @response = calculate(query)
   end
 
-  def days_present_in_preceeding_year
-    response['persons'][person_name]['days_present_in_new_zealand_in_preceeding_year']
+  def meets_minimum_presence_requirements
+    @response['persons'][person_name]['citizenship__meets_minimum_presence_requirements'][@day]
+  end
+
+  def meets_each_year_minimum_presence_requirements
+    @response['persons'][person_name]['citizenship__meets_each_year_minimum_presence_requirements'][@day]
+  end
+
+  def meets_5_year_presence_requirement
+    @response['persons'][person_name]['citizenship__meets_5_year_presence_requirement'][@day]
+  end
+
+  def days_by_rolling_year
+    @response['persons'][person_name]['days_present_in_new_zealand_in_preceeding_year']
   end
 
   def query
     {
       'persons' => {
         person_name => {
+          'citizenship__meets_minimum_presence_requirements' => {
+            @day => nil
+          },
           'citizenship__meets_each_year_minimum_presence_requirements' => {
             @day => nil
           },
           'citizenship__meets_5_year_presence_requirement' => {
             @day => nil
           },
-          'citizenship__meets_each_year_minimum_presence_requirements' => {
-            @day => nil
-          },
           'days_present_in_new_zealand_in_preceeding_year' => {
-            @day => nil
+            @day => nil,
+            years_before(1) => nil,
+            years_before(2) => nil,
+            years_before(3) => nil,
+            years_before(4) => nil
           },
           'present_in_new_zealand' => presence_values
         }
@@ -48,7 +63,6 @@ class EligibilityService
   end
 
   def presence_values
-    date_format = '%Y-%m-%d'
     presence = {}
 
     @client.movements.order(:carrier_date_time).each do |movement|
@@ -69,6 +83,13 @@ class EligibilityService
   # def presence_by_day
   #    { @day: @client.present_in_nz? }
   # end
+  def date_format
+    '%Y-%m-%d'
+  end
+
+  def years_before(num_years)
+    (@day.to_date - num_years.year).strftime(date_format)
+  end
 
   def person_name
     'Ruby'
