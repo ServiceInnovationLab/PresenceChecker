f237ae6b22ebddad9a96de6624d9606aa4729a00
Brenda Wallace <brenda.wallace@dia.govt.nz>
2018-12-11 17:12:13 +1300

    Handle same day return, and next day return

--- app/services/eligibility_service.rb
+++ app/services/eligibility_service.rb
@@ -84,8 +84,7 @@ class EligibilityService
         # today they're here, tomorrow they're not
         # today = movement.carrier_date_time.to_date.strftime(date_format)
         tomorrow = (movement.carrier_date_time.to_date + 1).strftime(date_format)
-        # presence[today] = true
-        presence[tomorrow] = false
+        presence[tomorrow] = false unless @client.arrived_on_this_day?(tomorrow)
       end
     end
 
