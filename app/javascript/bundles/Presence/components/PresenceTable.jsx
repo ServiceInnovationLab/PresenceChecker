f3b69d674d7eaa0ecb261db55c2165562b087a0d
Joris Rotteveel <joris.rotteveel@touchtech.co.nz>
2018-12-11 18:17:34 +1300

    ADDED: a rolling year table

--- app/javascript/bundles/Presence/components/PresenceTable.jsx
+++ app/javascript/bundles/Presence/components/PresenceTable.jsx
@@ -1,30 +1,46 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-
+import { sum } from 'lodash'
 import Year from './Year';
 import Table from '../../../components/Table';
 
 export default class PresenceTable extends React.Component {
-  static propTypes = { years: PropTypes.arrayOf(PropTypes.object) };
+  static propTypes = { rollingYearData: PropTypes.object };
 
   render() {
-    const { years, isEligible, totalDays } = this.props;
+    const { rollingYearData } = this.props;
+    const firstKey = Object.keys(rollingYearData)[0];
+    const yearData = rollingYearData[firstKey];
+    const isEligible = yearData.meetsMinimumPresence;
+    const totalDays = sum(yearData.daysInNZ);
+    // create a 'years' array that combines the days and if the person was eligible for that period
+
+    const years = yearData.last5Years.map((isEligible, index) => {
+      return {
+        year: yearData.last5Years.length - index,
+        daysPresent: yearData.daysInNZ[index],
+        meetsAmountOfDaysInNZ: isEligible,
+        //we need to get rid of the periodsAway, this is just to get it working
+        periodsAway: []
+      }
+    })
 
     return (
       <div className="results">
         <Table className="presence-table" cellPadding="0" cellSpacing="0">
-          <tr colSpan="4">
-            <th>
+          <tr>
+            <th className="is-header">
               <header className={isEligible ? '' : 'has-error'}>
-                <span>Total days: {totalDays}</span>
+                <span>Total days in NZ: {totalDays}</span>
                 <i className={`fas ${isEligible ? 'fa-check' : 'fa-times'}`} />
               </header>
             </th>
+            <th>&nbsp;</th>
           </tr>
           {years.map((year, index) => {
             return (
-              <tr key={index} className="">
-                <td>
+              <tr key={index}  className="">
+                <td colSpan="2">
                   <Year {...year} />
                 </td>
               </tr>
--- app/javascript/bundles/Presence/components/Year.jsx
+++ app/javascript/bundles/Presence/components/Year.jsx
@@ -24,27 +24,17 @@ export default class Year extends React.Component {
     const { isCollapsed } = this.state;
     return (
       <Table
-        className={`year-table is-text-center`}
+        className={`year-table is-text-center ${meetsAmountOfDaysInNZ
+          ? ''
+          : 'has-error'}`}
         cellPadding="0"
         cellSpacing="0"
       >
-        <tr>
+        <tr >
           <td className="header-container has-bottom-border has-right-border">
-            <h3 className="is-dark">{year}</h3>
+            <h3>Yr {year}</h3>
           </td>
-          <td className="has-bottom-border">{daysPresent}</td>
-          <td className="has-bottom-border">
-            <button
-              className="btn light"
-              onClick={() =>
-                this.setState({
-                  isCollapsed: !this.state.isCollapsed
-                })}
-            >
-              <span>details</span>
-              <i className={`fas fa-chevron-${isCollapsed ? 'down' : 'up'} `} />
-            </button>
-          </td>
+          <td className="has-bottom-border">add dates here</td>
           <td
             className={`icon-container has-bottom-border ${meetsAmountOfDaysInNZ
               ? ''
@@ -57,37 +47,10 @@ export default class Year extends React.Component {
             />
           </td>
         </tr>
-        <tr>
-          <td colSpan="4">
-            <Table className="details-table">
-              <tr className={`is-light ${isCollapsed ? 'u-hide' : ''}`}>
-                <td
-                  colSpan="2"
-                  className="secondary-header has-bottom-border has-border--light"
-                >
-                  <h3 className="is-dark">Travel Days</h3>
-                </td>
-              </tr>
-              {periodsAway.map(({ start, end, total }, index) => {
-                return (
-                  <tr
-                    key={index}
-                    className={`is-light is-text-left date-range ${isCollapsed
-                      ? 'u-hide'
-                      : ''}`}
-                  >
-                    <td className="date-range__dates">
-                      <h3 className="is-dark">
-                        {start} - {end}
-                      </h3>{' '}
-                    </td>
-                    <td className="date-range__total">{total} days</td>
-                  </tr>
-                );
-              })}
-            </Table>
-          </td>
+        <tr >
+          <th colSpan="3" className="has-bottom-border is-light year-table__summary" >Days in NZ: {daysPresent}</th>
         </tr>
+
       </Table>
     );
   }
