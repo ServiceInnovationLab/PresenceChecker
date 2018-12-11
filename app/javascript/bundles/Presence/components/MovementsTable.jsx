f9c8dddcb8db743e04567f0f1aa5006fe33b70b0
Awesome Code <team@xinminlabs.com>
2018-12-11 22:50:27 +0000

    Auto corrected by following Javascript Space

--- app/javascript/bundles/Presence/components/MovementsTable.jsx
+++ app/javascript/bundles/Presence/components/MovementsTable.jsx
@@ -4,7 +4,7 @@ import { format } from 'date-fns';
 class MovementsTable extends React.Component {
 
   render() {
-    const {movements} = this.props;
+    const { movements } = this.props;
     return (
       <div>
         <table className="presence-table movements-table" cellpadding="0" cellspacing="0">
@@ -21,7 +21,7 @@ class MovementsTable extends React.Component {
                 <table className="year-table is-text-center" cellpadding="0" cellspacing="0">
                   <tbody>
                     <tr>
-                      <td style={{width: '20%'}} className="header-container has-bottom-border has-right-border">
+                      <td style={{ width: '20%' }} className="header-container has-bottom-border has-right-border">
                         <h3 className="is-dark">{item.direction.charAt(0).toUpperCase() + item.direction.slice(1)}</h3>
                       </td>
                       <td className="has-bottom-border">{format(item.carrier_date_time, 'DD MMM YYYY')}</td>
