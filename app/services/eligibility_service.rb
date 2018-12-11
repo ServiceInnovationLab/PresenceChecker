71d1796bb81d0e7e5276cb4361fe1e4dc3a493b2
Brenda Wallace <brenda.wallace@dia.govt.nz>
2018-12-11 17:20:15 +1300

    Handles departure and arrival on sameday as all present

--- app/services/eligibility_service.rb
+++ app/services/eligibility_service.rb
@@ -97,9 +97,9 @@ class EligibilityService
         presence[movement.carrier_date_time.to_date.strftime(date_format)] = true
       elsif movement.direction == 'departure'
         # today they're here, tomorrow they're not
-        # today = movement.carrier_date_time.to_date.strftime(date_format)
+        today = movement.carrier_date_time.to_date.strftime(date_format)
         tomorrow = (movement.carrier_date_time.to_date + 1).strftime(date_format)
-        presence[tomorrow] = false unless @client.arrived_on_this_day?(tomorrow)
+        presence[tomorrow] = false unless @client.arrived_on_this_day?(today) || @client.arrived_on_this_day?(tomorrow) 
       end
     end
 
