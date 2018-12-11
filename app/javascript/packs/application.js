9cf8d2434c47c8e09461f955601668f642a15bf3
danieljauch <daniel.p.jauch@gmail.com>
2018-12-11 13:09:17 +1300

    This sets all the logic into a single element so that the view render is simpler

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,14 +1,28 @@
+import React from 'react';
 import ReactOnRails from 'react-on-rails';
 
 import Identity from '../bundles/Identity/components/Identity';
 import EligibilityDates from '../components/EligibilityDates';
-import PresenceDebugger from '../components/PresenceDebugger';
 import PresenceTable from '../bundles/Presence/components/PresenceTable';
 
-// This is how react_on_rails can see the HelloWorld in the browser.
+class ShowClient extends React.Component {
+  render() {
+    const { clientId, identities, eligibleDateRanges, isEligible, totalDays, years } = this.props;
+
+    return (
+      <main role="main">
+        <section className="identities-wrapper">
+          <Identity id={clientId} identities={identities} />
+        </section>
+        <section className="dates-wrapper">
+          <EligibilityDates eligibleDateRanges={eligibleDateRanges} />
+          <PresenceTable isEligible={isEligible} totalDays={totalDays} years={years} />
+        </section>
+      </main>
+    );
+  }
+}
+
 ReactOnRails.register({
-  Identity,
-  EligibilityDates,
-  PresenceDebugger,
-  PresenceTable
+  ShowClient
 });
