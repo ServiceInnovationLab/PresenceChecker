35afebcb70e49fd4f43a4dbcade96f39b48409c9
Dana Iti <dana.iti.work@gmail.com>
2018-12-11 14:15:23 +1300

    Added more movements

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,36 +31,77 @@ end
 
 
 
-
-
-
 #### Lee Jun-fan ####
 
 Country.create(id: 1, name: "United States of America")
 Client.create(id: 1, im_client_id: "12345", file_number: "1")
-Identity.create(id: 1, client_id: 1, identity_number: "Y-3183346-E", family_name: "Lee", first_name: "Jun-fan", second_name: "", third_name: "", gender: "Male", country_of_birth_id: 1, nationality: "United State", issuing_state_id: 1, serial_number: "Y-3775136-E")
+Identity.create(id: 1, client_id: 1, identity_number: "Y-3183346-E", family_name: "Lee", first_name: "Bruce", second_name: "", third_name: "", gender: "Male", country_of_birth_id: 1, nationality: "United State", issuing_state_id: 1, serial_number: "Y-3775136-E")
 
 Country.create(id: 2, name: "Hong Kong")
-Client.create(id: 1, im_client_id: "12345", file_number: "1")
-Identity.create(id: 2, client_id: 1, identity_number: "Y-3183347-E", amily_name: "Lee", first_name: "Jun-fan", second_name: "", third_name: "", gender: "Male", country_of_birth_id: 1, nationality: "Hong Kong", issuing_state_id: 2, serial_number: "Y-3775137-E")
+Identity.create(id: 2, client_id: 1, identity_number: "Y-3183347-E", family_name: "Lee", first_name: "Jun-fan", second_name: "", third_name: "", gender: "Male", country_of_birth_id: 1, nationality: "Hong Kong", issuing_state_id: 2, serial_number: "Y-3775137-E")
 
-# Year 1
-Movement.create(id: 1, identity_id: 1, direction: "arrival", carrier_date_time: "2014-12-10 20:45:42")
-Movement.create(id: 1, identity_id: 1, direction: "departure", carrier_date_time: "2014-22-10 20:45:42")
-Movement.create(id: 1, identity_id: 2, direction: "arrival", carrier_date_time: "2014-12-10 20:45:42")
-Movement.create(id: 1, identity_id: 2, direction: "departure", carrier_date_time: "2014-22-10 20:45:42")
-# Year 2
-Movement.create(id: 1, identity_id: 1, direction: "arrival", carrier_date_time: "2015-12-10 20:45:42")
-Movement.create(id: 1, identity_id: 1, direction: "departure", carrier_date_time: "2015-22-10 20:45:42")
-# Year 3
-Movement.create(id: 1, identity_id: 1, direction: "arrival", carrier_date_time: "2016-12-10 20:45:42")
-Movement.create(id: 1, identity_id: 1, direction: "departure", carrier_date_time: "2016-22-10 20:45:42")
-# Year 4
-Movement.create(id: 1, identity_id: 1, direction: "arrival", carrier_date_time: "2017-12-10 20:45:42")
-Movement.create(id: 1, identity_id: 1, direction: "departure", carrier_date_time: "2017-22-10 20:45:42")
-# Year 5
-Movement.create(id: 1, identity_id: 1, direction: "arrival", carrier_date_time: "2018-12-10 20:45:42")
-Movement.create(id: 1, identity_id: 1, direction: "departure", carrier_date_time: "2018-22-10 20:45:42")
+# Identity 1
+Movement.create(identity_id: 1, direction: "departure", carrier_date_time: "1997-09-26 20:00:00")
+Movement.create(identity_id: 1, direction: "arrival", carrier_date_time: "1997-10-18 06:00:00")
+Movement.create(identity_id: 1, direction: "departure", carrier_date_time: "1997-11-05 06:00:00")
+Movement.create(identity_id: 1, direction: "arrival", carrier_date_time: "1999-11-08 23:00:00")
+Movement.create(identity_id: 1, direction: "departure", carrier_date_time: "2000-02-27 09:00:00")
+Movement.create(identity_id: 1, direction: "arrival", carrier_date_time: "2000-02-28 23:00:00")
+Movement.create(identity_id: 1, direction: "departure", carrier_date_time: "2000-04-21 17:55:00")
+Movement.create(identity_id: 1, direction: "arrival", carrier_date_time: "2000-04-28 16:45:00")
+Movement.create(identity_id: 1, direction: "departure", carrier_date_time: "2000-08-20 09:45:00")
+Movement.create(identity_id: 1, direction: "arrival", carrier_date_time: "2000-08-22 23:45:00")
+Movement.create(identity_id: 1, direction: "departure", carrier_date_time: "2000-11-15 14:00:00")
+Movement.create(identity_id: 1, direction: "arrival", carrier_date_time: "2000-11-20 23:00:00")
+
+# Identity 2
+Movement.create(identity_id: 2, direction: "departure", carrier_date_time: "2011-02-20 16:15:00")
+Movement.create(identity_id: 2, direction: "arrival", carrier_date_time: "2011-02-24 23:00:00")
+Movement.create(identity_id: 2, direction: "departure", carrier_date_time: "2011-06-18 11:30:00")
+Movement.create(identity_id: 2, direction: "arrival", carrier_date_time: "2011-06-26 19:45:00")
+Movement.create(identity_id: 2, direction: "departure", carrier_date_time: "2012-03-16 11:30:00")
+Movement.create(identity_id: 2, direction: "arrival", carrier_date_time: "2012-06-23 19:05:00")
+Movement.create(identity_id: 2, direction: "departure", carrier_date_time: "2012-06-01 18:00:00")
+Movement.create(identity_id: 2, direction: "arrival", carrier_date_time: "2012-06-07 03:10:00")
+Movement.create(identity_id: 2, direction: "departure", carrier_date_time: "2012-08-04 20:45:00")
+Movement.create(identity_id: 2, direction: "arrival", carrier_date_time: "2012-08-30 05:15:00")
+Movement.create(identity_id: 2, direction: "departure", carrier_date_time: "2013-11-21 10:25:00")
+Movement.create(identity_id: 2, direction: "arrival", carrier_date_time: "2013-12-08 09:00:00")
+Movement.create(identity_id: 2, direction: "departure", carrier_date_time: "2014-04-02 21:45:00")
+Movement.create(identity_id: 2, direction: "arrival", carrier_date_time: "2014-04-19 05:15:00")
+Movement.create(identity_id: 2, direction: "departure", carrier_date_time: "2015-10-07 17:10:00")
+Movement.create(identity_id: 2, direction: "arrival", carrier_date_time: "2015-10-13 07:30:00")
+Movement.create(identity_id: 2, direction: "departure", carrier_date_time: "2015-11-08 09:30:00")
+Movement.create(identity_id: 2, direction: "arrival", carrier_date_time: "2015-11-13 00:30:00")
+Movement.create(identity_id: 2, direction: "departure", carrier_date_time: "2016-11-14 08:30:00")
+Movement.create(identity_id: 2, direction: "arrival", carrier_date_time: "2016-11-21 23:00:00")
+Movement.create(identity_id: 2, direction: "departure", carrier_date_time: "2017-09-22 13:00:00")
+Movement.create(identity_id: 2, direction: "arrival", carrier_date_time: "2017-08-28 23:45:00")
+Movement.create(identity_id: 2, direction: "departure", carrier_date_time: "2017-11-06 09:30:00")
+Movement.create(identity_id: 2, direction: "arrival", carrier_date_time: "2017-11-11 23:25:00")
+Movement.create(identity_id: 2, direction: "departure", carrier_date_time: "2017-11-29 09:00:00")
+Movement.create(identity_id: 2, direction: "arrival", carrier_date_time: "2017-11-30 16:30:00")
+Movement.create(identity_id: 2, direction: "departure", carrier_date_time: "2018-04-28 05:50:00")
+Movement.create(identity_id: 2, direction: "arrival", carrier_date_time: "2018-04-29 23:30:00")
+Movement.create(identity_id: 2, direction: "departure", carrier_date_time: "2018-07-03 08:50:00")
+Movement.create(identity_id: 2, direction: "arrival", carrier_date_time: "2018-07-06 23:20:00")
+Movement.create(identity_id: 2, direction: "departure", carrier_date_time: "2018-09-28 06:40:00")
+Movement.create(identity_id: 2, direction: "arrival", carrier_date_time: "2018-10-03 23:45:00")
+
+# ###########################
+
+# # Su Sua
+# # From Samoa
+
+# ###########################
+
+
+# Country.create(id: 4, name: "Samoa")
+# Client.create(id: 2, im_client_id: "12345", file_number: "1")
+# Identity.create(id: 1, client_id: 1, identity_number: "Y-3183346-E", family_name: "Sua", first_name: "Su", second_name: "", third_name: "", gender: "Female", country_of_birth_id: 1, nationality: "United State", issuing_state_id: 1, serial_number: "Y-3775136-E")
+
+# # Year 1
+# Movement.create(identity_id: 1, direction: "arrival", carrier_date_time: "2018-08-24 20:45:42")
 
 
 
