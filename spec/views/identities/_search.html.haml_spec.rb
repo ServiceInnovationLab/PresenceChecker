824091c3b95e67c4ec184d881450ca8d3acd2fde
Dana Iti <dana.iti.work@gmail.com>
2018-12-12 16:00:57 +1300

    added test for search

--- spec/views/identities/_search.html.haml_spec.rb
+++ spec/views/identities/_search.html.haml_spec.rb
@@ -4,8 +4,9 @@ require 'rails_helper'
 
 RSpec.describe 'identities/_search', type: :view do
   describe 'search view' do
-    it 'displays a search field' do
+    it 'displays a search form' do
       render
+      expect(rendered).to have_selector('form[id="search-form"]', count: 1)
     end
   end
 end
