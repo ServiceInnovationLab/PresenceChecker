cb303431b7eeb6eb45adfb5178fe48b7575a781e
Dana Iti <dana.iti.work@gmail.com>
2018-12-13 15:26:49 +1300

    test for application view

---
+++ spec/views/layouts/application.html.haml_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'layouts/application', type: :view do
+
+  include Devise::Test::ControllerHelpers
+
+  describe 'header view' do
+    let(:current_user) { FactoryBot.create :user }
+
+    before do
+      assign(:users, current_user)
+      sign_in current_user
+      render
+    end
+
+    it 'displays the application page' do
+      render
+      expect(rendered).to have_selector('div[class="container"]')
+    end
+  end
+end
