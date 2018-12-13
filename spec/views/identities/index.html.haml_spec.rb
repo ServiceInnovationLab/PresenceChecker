827ced2567c4b239bbacbf02b4d2818e56ef8e3c
Dana Iti <dana.iti.work@gmail.com>
2018-12-13 13:56:15 +1300

    identities spec

--- spec/views/identities/index.html.haml_spec.rb
+++ spec/views/identities/index.html.haml_spec.rb
@@ -1,17 +1,3 @@
-# # frozen_string_literal: true
-
-# require 'rails_helper'
-
-# RSpec.describe 'identities/index', type: :view do
-#   describe 'identities view' do
-#     let(:identity) { FactoryBot.create :identity }
-#     it 'lists a number of identities' do
-#       expect(identity).to be_truthy
-#       expect(identity.first_name).to_not be_empty
-#     end
-#   end
-# end
-
 # frozen_string_literal: true
 
 require 'rails_helper'
