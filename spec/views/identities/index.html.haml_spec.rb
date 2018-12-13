d165391a06de928304e065c88edf5b43752bac69
Dana Iti <dana.iti.work@gmail.com>
2018-12-13 13:53:09 +1300

    identities test

--- spec/views/identities/index.html.haml_spec.rb
+++ spec/views/identities/index.html.haml_spec.rb
@@ -1,13 +1,31 @@
+# # frozen_string_literal: true
+
+# require 'rails_helper'
+
+# RSpec.describe 'identities/index', type: :view do
+#   describe 'identities view' do
+#     let(:identity) { FactoryBot.create :identity }
+#     it 'lists a number of identities' do
+#       expect(identity).to be_truthy
+#       expect(identity.first_name).to_not be_empty
+#     end
+#   end
+# end
+
 # frozen_string_literal: true
 
 require 'rails_helper'
 
 RSpec.describe 'identities/index', type: :view do
-  describe 'identities view' do
-    let(:identity) { FactoryBot.create :identity }
-    it 'lists a number of identities' do
-      expect(identity).to be_truthy
-      expect(identity.first_name).to_not be_empty
-    end
+  let(:client) { FactoryBot.create(:client) }
+
+  before do
+    FactoryBot.create_list(:identity, 2)
+    assign(:client, client)
+    assign(:identities, client.identities)
+    render
   end
+
+  it { expect(rendered).to include 'Identities' }
+  it { expect(rendered).to include 'Passport No.' }
 end
