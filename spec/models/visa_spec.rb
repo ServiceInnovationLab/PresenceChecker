3514dea2b9c5a5cfb0a17e020f7b8246d803f466
Lyall Morrison <lyall@rabidtech.co.nz>
2019-02-25 12:45:09 +1300

    improve spec output readability

--- spec/models/visa_spec.rb
+++ spec/models/visa_spec.rb
@@ -9,16 +9,16 @@ RSpec.describe Visa, type: :model do
     expect(subject).to be_valid
   end
 
-  context 'the VisaType is not indefinite' do
+  context 'when the VisaType is not indefinite' do
     let(:visa_type) { FactoryBot.build(:visa_type, indefinite: false) }
 
-    context 'the expiry date is set' do
+    context 'and the expiry date is set' do
       let(:expiry_date) { Time.zone.today + 2.years }
       subject { FactoryBot.build(:visa, visa_type: visa_type, expiry_date: expiry_date) }
       it { should be_valid }
     end
 
-    context 'the expiry date is nil' do
+    context 'and the expiry date is nil' do
       let(:expiry_date) { nil }
       subject { FactoryBot.build(:visa, visa_type: visa_type, expiry_date: expiry_date) }
       it { should be_invalid }
