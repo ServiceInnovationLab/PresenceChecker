191d19d1a287251f3c6e47660e3f987f083d5df2
Lyall Morrison <lyall@rabidtech.co.nz>
2019-02-28 16:27:22 +1300

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -101,7 +101,6 @@ ActiveRecord::Schema.define(version: 2019_02_28_021132) do
   create_table "visa_types", force: :cascade do |t|
     t.string "visa_type", null: false
     t.string "description", null: false
-    t.boolean "indefinite", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
