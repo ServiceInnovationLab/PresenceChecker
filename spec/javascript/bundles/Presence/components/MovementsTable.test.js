577c69dcf43106121795c6ddb557ab287b1edf80
Dana Iti <dana.iti.work@gmail.com>
2019-03-04 15:50:21 +1300

    added, styles to show indefinite visa rows and  tests

--- spec/javascript/bundles/Presence/components/MovementsTable.test.js
+++ spec/javascript/bundles/Presence/components/MovementsTable.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { configure, shallow } from 'enzyme';
+import { configure, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import renderer from 'react-test-renderer';
 
 import MovementsTable from '../../../../../app/javascript/bundles/Presence/components/MovementsTable';
 
@@ -8,10 +9,48 @@ configure({ adapter: new Adapter() });
 
 describe('<MovementsTable />', () => {
   let props = {
-    movements: [],
+    movements: [
+      {
+        carrier_date_time: '2018-02-12T00:00:00.000Z',
+        created_at: '2019-03-03T22:55:25.179Z',
+        direction: 'arrival',
+        id: 93,
+        identity_id: 13,
+        updated_at: '2019-03-03T22:55:25.179Z',
+        visa_type: 'R'
+      },
+      {
+        carrier_date_time: '2018-01-20T00:00:00.000Z',
+        created_at: '2019-03-03T22:55:25.177Z',
+        direction: 'departure',
+        id: 92,
+        identity_id: 13,
+        updated_at: '2019-03-03T22:55:25.177Z',
+        visa_type: null
+      }
+    ],
   };
 
   it('renders without error', () => {
-    shallow(<MovementsTable {...props} />);
+    mount(<MovementsTable {...props} />);
   });
+
+  it('renders correctly without failing', () => {
+    const tree = renderer
+      .create(<MovementsTable {...props} />)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  // test or nil
+  it('should show indefinite visa type if positive class name exists', () => {
+    const wrapper = mount(<MovementsTable {...props} />);
+    expect(wrapper.find('.movements-table-highlight--positive').childAt(2).text()).toBe('R');
+  });
+
+  it('should show no visa type if negative class name exists', () => {
+    const wrapper = mount(<MovementsTable {...props} />);
+    expect(wrapper.find('.movements-table-highlight--negative').childAt(2).text()).toBe('');
+  });
+
 });
