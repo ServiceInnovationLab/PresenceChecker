375ec28fc4a40db767b6ec1649ce8dd8096a14e1
Dana Iti <dana.iti.work@gmail.com>
2019-03-11 13:41:15 +1300

    update methods

--- spec/models/movement_spec.rb
+++ spec/models/movement_spec.rb
@@ -3,9 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe Movement, type: :model do
-  subject { FactoryBot.build(:movement) }
+  subject { FactoryBot.build(:movement, direction: 'arrival') }
+  let(:arrival) { FactoryBot.create(:movement, direction: 'A') }
+  let(:departure) { FactoryBot.create(:movement, direction: 'D') }
 
   it 'has a factory which produces valid models by default' do
     expect(subject).to be_valid
+  end
+
+  it 'recognises when movement direction is A or D' do
+    expect(arrival).to be_valid
+    expect(departure).to be_valid
   end
 end
