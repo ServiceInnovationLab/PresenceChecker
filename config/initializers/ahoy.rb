eb974b1fc3a8bdcb27127b5fa8b15fe6e06a132a
JacOng17 <jacob_ong88@hotmail.com>
2019-03-12 10:38:44 +1300

    Added ahoy gem

---
+++ config/initializers/ahoy.rb
@@ -0,0 +1,12 @@
+class Ahoy::Store < Ahoy::DatabaseStore
+end
+
+# set to true for JavaScript tracking
+Ahoy.api = true
+
+# better user agent parsing
+Ahoy.user_agent_parser = :device_detector
+
+# better bot detection
+Ahoy.bot_detection_version = 2
+
